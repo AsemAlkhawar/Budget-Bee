74d0f57100d2778d040bd373a974fe13d55c9a2d
Asem Alkhawar <asemhisham7@gmail.com>
2024-06-21 17:42:32 -0700

    Started working on backend!

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const cors = require('cors');
 const config = require('./config');
+const cors = require('cors');
 const app = express();
 const port = 3001;
 
@@ -11,15 +11,20 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 mongoose.connect(config.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    auth: {
+        authSource: "admin" // Usually, admin is used as the authentication database
+    },
     user: config.mongoUser,
     pass: config.mongoPassword
 });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
