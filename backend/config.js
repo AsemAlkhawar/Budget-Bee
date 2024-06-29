489180a09c5ffbbd124ac4eee0b642dca12a7a34
asemhisham1 <asemhisham7@gmail.com>
2024-06-28 20:11:21 -0700

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mongoURI: '',
+    mongoUser: '',
+    mongoPassword: '',
+    plaidClientID: '',
+    plaidSecret: '',
+    plaidEnv: '',  // Can be 'sandbox', 'development', or 'production'
+    openaiApiKey: ''
+};
